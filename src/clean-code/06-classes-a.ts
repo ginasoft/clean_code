267104b7daf0f2b555be6b62141580aa4c9358ea
Natalia Cuellas <nataliagcuellas@gmail.com>
2025-10-06 17:41:24 -0300

    aplicando el principio de responsabilidad unica, priorizando la composicion frente a la herencia

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,65 @@
+(() => {
+
+    // No se está aplicando el principio de responsabilidad única
+
+    type Gender = 'M'|'F';
+
+    class Person {
+        constructor(
+            public name: string, 
+            public gender: Gender, 
+            public birthdate: Date
+        ){}
+    }
+
+
+    class User extends Person {
+        
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super( name, gender, birthdate );
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder  : string,
+            email                  : string,
+            role                   : string,
+            name                   : string,
+            gender                 : Gender,
+            birthdate              : Date
+        ) {
+            super(email, role, name, gender, birthdate );
+        }
+    }
+
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'natalia@mail.com',
+        'Admin',
+        'Natalia',
+        'F',
+        new Date('1995-01-01')
+    );
+
+    console.log({ userSettings });
+
+
+})();
