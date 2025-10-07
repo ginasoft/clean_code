90d0aabbfe54174e2fc63277224391bd5b40a64e
Natalia Cuellas <nataliagcuellas@gmail.com>
2025-10-06 21:13:24 -0300

    remove dependency axios

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,10 +1,16 @@
-import axios from "axios";
-
+//import axios from "axios";
+// yarn remove axios ó npm remove axios
 export class HttpClient {
 
-        async get( url: string ) {
-            const { data, status } = await axios.get(url);
-            return { data, status };
-        }
+     //   async get( url: string ) {
+     //       const { data, status } = await axios.get(url);
+     //       return { data, status };
+     //   }
 
+    async get ( url: string ){
+        const resp = await fetch( url );
+        const data = await resp.json();
+
+        return { data: '', status: resp.status }; 
+    } 
 }
